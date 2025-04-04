8e801fa89fe4cb075fc603d8858b32be9335bc7d
atharvakulk02 <156200102+atharvakulk02@users.noreply.github.com>
2025-04-04 13:30:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Serve frontend static files
-app.use(express.static(path.join(__dirname, "../spyfall_frontend/build")));
+//app.use(express.static(path.join(__dirname, "../spyfall_frontend/build")));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../spyfall_frontend/build", "index.html"));
-});
+//app.get("*", (req, res) => {
+   // res.sendFile(path.join(__dirname, "../spyfall_frontend/build", "index.html"));
+//});
 
 // Create an HTTP server and attach WebSocket to it
 const server = http.createServer(app);
